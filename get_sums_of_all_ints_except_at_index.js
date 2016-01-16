4e90810abe07e62481a1ae8ac7154365dac48269
steph-harris <steph.harris21@gmail.com>
2016-01-16 10:52:25 -0500

    initial commit with the beginning of our get sums except for index js

---
+++ get_sums_of_all_ints_except_at_index.js
@@ -0,0 +1,13 @@
+var integers = process.argv
+
+//take in 4 numbers
+//take 3 of 4 and add them (not the one at the index)
+//run loop, making sure that the same 3 aren't being repeated
+//return the values in an array
+// make a function that keeps returned valus from being equal 
+// for loop(at this position[i] skip this number and add the rest together)
+
+for(i=2; i<integers.length; i++){
+  var parsedArg = parseFloat(integers[i]);
+  console.log(skipExtraArgs);
+}
